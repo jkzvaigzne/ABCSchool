5c3c8c3e3d3c4d1a526588887e474e288fa0fd3d
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-12 19:19:09 +0200

    Base Db Context - Identity Models

---
+++ Infrastructure/Contexts/BaseDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
+using Infrastructure.Identity.Models;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+    public abstract class BaseDbContext :
+        MultiTenantIdentityDbContext<
+            ApplicationUser,
+            ApplicationRole,
+            string,
+            IdentityUserClaim<string>,
+            IdentityUserRole<string>,
+            IdentityUserLogin<string>,
+            ApplicationRoleClaim,
+            IdentityUserToken<string>>
+    {
+        protected BaseDbContext(IMultiTenantContextAccessor<ABCSchoolTenantInfo> tenantInfoContextAccessor, DbContextOptions options) 
+            : base(tenantInfoContextAccessor, options)
+        {
+        }
+    }
+}
