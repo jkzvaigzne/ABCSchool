6805c57b9cfb2948f633ccdf009c633ce20b7dc3
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-12 11:43:14 +0200

    Multitenant into Dependency Injection

---
+++ Infrastructure/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Finbuckle.MultiTenant;
+using Infrastructure.Tenancy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class Startup
+    {
+        public static IServiceCollection AddMultitenancyServices(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            return services
+                .AddDbContext<TenantDbContext>(options => options
+                    .UseSqlServer(config.GetConnectionString("DefaultConnection")))
+                .AddMultiTenant<ABCSchoolTenantInfo>()
+                    .WithHeaderStrategy("tenant")
+                    .WithClaimStrategy("tenant")
+                    .WithEFCoreStore<TenantDbContext, ABCSchoolTenantInfo>()
+                    .Services;
+        }
+    }
+}
