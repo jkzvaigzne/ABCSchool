211729d5f8c4fd1a620854932283cd4f3ae426f4
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-12 11:35:03 +0200

    Tenant Db Context

---
+++ Infrastructure/Tenancy/TenantDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Finbuckle.MultiTenant.EntityFrameworkCore.Stores.EFCoreStore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tenancy
+{
+    public class TenantDbContext(DbContextOptions<TenantDbContext> options) 
+        : EFCoreStoreDbContext<ABCSchoolTenantInfo>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ABCSchoolTenantInfo>()
+                .ToTable("Tenants", "MultiTenancy");
+        }
+    }
+}
