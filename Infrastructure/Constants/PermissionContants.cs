8cb6328964c15683dee6f8a1c54922c1cd9bfeea
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-14 18:41:52 +0200

    School Feature Constants

--- Infrastructure/Constants/PermissionContants.cs
+++ Infrastructure/Constants/PermissionContants.cs
@@ -8,4 +8,14 @@
         public const string Delete = nameof(Delete);
         public const string UpgradeSubscription = nameof(UpgradeSubscription);
     }
+
+    public static class SchoolFeature
+    {
+        public const string Tenants = nameof(Tenants);
+        public const string Users = nameof(Users);
+        public const string Roles = nameof(Roles);
+        public const string UserRoles = nameof(UserRoles);
+        public const string RoleClaims = nameof(RoleClaims);
+        public const string Schools = nameof(Schools);
+    }
 }
