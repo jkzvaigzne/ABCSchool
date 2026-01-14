cf628c75cc1c2a1890c0380a2592a23d4f18f853
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-14 18:38:42 +0200

    School Action Constants

---
+++ Infrastructure/Constants/PermissionContants.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Constants
+{
+    public static class SchoolAction
+    {
+        public const string Read = nameof(Read);
+        public const string Create = nameof(Create);
+        public const string Update = nameof(Update);
+        public const string Delete = nameof(Delete);
+        public const string UpgradeSubscription = nameof(UpgradeSubscription);
+    }
+}
