6a147739d69480ebac475c2f8588c592546ade1b
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-15 20:37:14 +0200

    Initialize Database - Migrations

---
+++ Infrastructure/Constants/ApplicationDbSeeder.cs
@@ -0,0 +1,37 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using Infrastructure.Contexts;
+using Infrastructure.Identity.Models;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Constants
+{
+    public class ApplicationDbSeeder(
+        IMultiTenantContextAccessor<ABCSchoolTenantInfo> tenantContextAccessor,
+        RoleManager<ApplicationRole> roleManager,
+        UserManager<ApplicationUser> userManager,
+        ApplicationDbContext applicationDbContext)
+    {
+        private readonly IMultiTenantContextAccessor<ABCSchoolTenantInfo> _tenantInfoContextAccessor = tenantContextAccessor;
+        private readonly RoleManager<ApplicationRole> _roleManager = roleManager;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
+
+        public async Task InitializeDatabaseAsync(CancellationToken cancellationToken)
+        {
+            if (_applicationDbContext.Database.GetMigrations().Any())
+            {
+                if ((await _applicationDbContext.Database.GetPendingMigrationsAsync(cancellationToken)).Any())
+                {
+                    await _applicationDbContext.Database.MigrateAsync(cancellationToken);
+                }
+
+                if (await _applicationDbContext.Database.CanConnectAsync(cancellationToken))
+                {
+                    // Seeding data
+                }
+            }
+        }
+    }
+}
